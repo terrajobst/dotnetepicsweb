cd64d904f3fdf3f04742aa6f3561faaf135d4b23
Immo Landwerth <immol@microsoft.com>
2020-12-02 17:24:12 -0800

    Ensure issues are sorted by kind, priority, cost and then title

--- src/ThemesOfDotNet/Data/Tree.cs
+++ src/ThemesOfDotNet/Data/Tree.cs
@@ -35,7 +35,42 @@ namespace ThemesOfDotNet.Data
             {
                 foreach (var child in parent.Children)
                     child.Parents.Add(parent);
-            }          
+
+                parent.Children.Sort((x, y) =>
+                {
+                    var result = x.Kind.CompareTo(y.Kind);
+                    if (result != 0)
+                        return result;
+
+                    if (x.Priority != null || y.Priority != null)
+                    {
+                        if (x.Priority == null)
+                            return 1;
+
+                        if (y.Priority == null)
+                            return -1;
+
+                        result = x.Priority.Value.CompareTo(y.Priority.Value);
+                        if (result != 0)
+                            return result;
+                    }
+
+                    if (x.Cost != null || y.Cost != null)
+                    {
+                        if (x.Cost == null)
+                            return 1;
+
+                        if (y.Cost == null)
+                            return -1;
+
+                        result = -x.Cost.Value.CompareTo(y.Cost.Value);
+                        if (result != 0)
+                            return result;
+                    }
+
+                    return x.Title.CompareTo(y.Title);
+                });
+            }           
         }
 
         public IReadOnlyCollection<TreeNode> Roots { get; set; }
